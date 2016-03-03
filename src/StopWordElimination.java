46d4de69d642697ef8199289d62ea0ca5d5569dc
Gherbi Hicham <gherbixhicham@gmail.com>
2016-03-03 23:04:38 +0100

    second commit

--- src/StopWordElimination.java
+++ src/StopWordElimination.java
@@ -1,14 +1,15 @@
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
+
+//This class is used to remove Stop Words and other Strings that are unrelated to the task of text classification
 
 public class StopWordElimination {
 	
